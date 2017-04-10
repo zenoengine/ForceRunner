2287bed0886cfa47b788ddf9e6be3c8a122cb5b5
ZenoAhn <zenoengine@gmail.com>
2017-04-11 03:17:00 +0900

    added missed file

---
+++ Assets/Script/TitleRoot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleRoot : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+        SoundManager.Instance.PlayMusic("bgm2",false);
+	}
+	
+    public void StartGame()
+    {
+        SceneManager.LoadScene("main");
+    }
+}
